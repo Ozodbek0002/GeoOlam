5f16fb49df15ec5a47936159a2e0acd4c7a934bd
Ozodbek <ozodbekozodov0002@gmail.com>
2023-01-03 22:25:15 +0500

    download

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -20,22 +20,13 @@ class BookController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         return view('admin.books.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         $data = new Book();
