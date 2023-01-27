b79bad8c96ff0296b78e0d396d9dab34637b5b73
Ozodbek <ozodbekozodov0002@gmail.com>
2023-01-27 11:47:02 +0500

    finish

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -13,17 +13,17 @@
 
 
         <title>GeoOlam</title>
-        <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
-        <link rel="stylesheet" href="assets/tether/tether.min.css">
-        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
-        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
-        <link rel="stylesheet" href="assets/dropdown/css/style.css">
-        <link rel="stylesheet" href="assets/animatecss/animate.css">
-        <link rel="stylesheet" href="assets/socicon/css/styles.css">
-        <link rel="stylesheet" href="assets/theme/css/style.css">
-        <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
-        <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+        <link rel="stylesheet" href="{{asset('assets/web/assets/mobirise-icons/mobirise-icons.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/tether/tether.min.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap-grid.min.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap-reboot.min.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/dropdown/css/style.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/animatecss/animate.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/socicon/css/styles.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/theme/css/style.css')}}">
+        <link rel="preload" as="style" href="{{asset('assets/mobirise/css/mbr-additional.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/mobirise/css/mbr-additional.css')}}" type="text/css">
 
 
     </head>
